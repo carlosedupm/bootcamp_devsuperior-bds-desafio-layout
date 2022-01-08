ef0b32df763a5d88a6afd75a543adabe9d13f3d0
carlosedupm <carlosedupm@gmail.com>
2022-01-08 20:31:47 -0300

    Estrutura basica estilizaçao

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+  return (
+    
+    
+    <button className="btn btn-icon">
+        <p>VER CATÁLOGO</p>
+      </button>
+      
+  );
+};
+
+export default ButtonIcon;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as MainImage } from 'assets/images/topcar.svg';
+import ButtonIcon from 'components/ButtonIcon';
 import NavBar from 'components/NavBar';
 
 import './styles.css';
@@ -11,12 +12,22 @@ const Home = () => {
         <div className="home-card">
           <div className="home-content-container">
             <h1>O carro perfeito para você</h1>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
           </div>
           <div className="home-image-container">
             <MainImage />
           </div>
         </div>
       </div>
+      <div className="btn-content-container">
+        <p>Comece agora a navegar</p>
+          <ButtonIcon />
+      </div>
+      
+      
     </>
   );
 };
